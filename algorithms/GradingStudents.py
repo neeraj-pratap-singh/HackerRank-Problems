8fdcbbe23e76fb7f02b1528904632d60357d71df
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-08-24 20:58:52 +0530

    Create GradingStudents.py

---
+++ algorithms/GradingStudents.py
@@ -0,0 +1,31 @@
+# HackerLand University has the following grading policy:
+
+# Every student receives a grade in the inclusive range from 0 to 100.
+# Any grade less than 40 is a failing grade.
+# Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+# If the difference between the  and the next multiple of 5 is less than 3 , round grade up to the next multiple of 5.
+# If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+# Examples
+
+#  grade = 84, round to  (85 - 84 is less than 3)
+#  grade = 29, do not round (result is less than 40)
+#  grade = 57, do not round (60 - 57 is 3 or higher)
+# Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+def round_grades(grades):
+    rounded_grades = []
+    for grade in grades:
+        if grade < 38:
+            rounded_grades.append(grade)
+        else:
+            next_multiple_of_5 = (grade // 5 + 1) * 5
+            if next_multiple_of_5 - grade < 3:
+                rounded_grades.append(next_multiple_of_5)
+            else:
+                rounded_grades.append(grade)
+    return rounded_grades
+
+# Example usage
+grades = [84, 29, 57]
+rounded_grades = round_grades(grades)
+print(rounded_grades)  # Output: [85, 29, 57]
